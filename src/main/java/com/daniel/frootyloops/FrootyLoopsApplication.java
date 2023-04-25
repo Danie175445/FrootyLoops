ac112dc4d3d42c2e817884b8db99d714669b1749
Daniel Castillo <danny4004@gmail.com>
2023-04-25 16:50:36 -0700

    first Commit

---
+++ src/main/java/com/daniel/frootyloops/FrootyLoopsApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.frootyloops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrootyLoopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrootyLoopsApplication.class, args);
+	}
+
+}
